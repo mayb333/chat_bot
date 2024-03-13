d045338e8b880b99ec78860c57e1260e52dd94ed
Sokolov Fedor <sokol_f4@mail.ru>
2024-03-14 01:28:37 +0300

    Configure endpoints

--- ml_service/src/routers/main.py
+++ ml_service/src/routers/main.py
@@ -8,12 +8,14 @@ from src.routers.schemas import Context, Prediction
 router = APIRouter()
 
 
-@router.get("/predict", response_model=Prediction)
-async def predict(query: str) -> Prediction:
-    return Prediction(text="Best answer", urls=["https://some_url_1.com", "https://some_url_2.com"])
-
-
 @router.get("/get_context", response_model=List[Context])
-async def context(query: str) -> List[Context]:
+async def get_context(query: str) -> List[Context]:
     return [Context(text="Some_Context_1", url="https://some_url_1.com"),
             Context(text="Some_Context_2", url="https://some_url_2.com")]
+
+
+@router.get("/predict", response_model=Prediction)
+async def predict(query: str) -> Prediction:
+    context = await get_context(query=query)
+    print(context)
+    return Prediction(text="Best answer", urls=["https://some_url_1.com", "https://some_url_2.com"])
